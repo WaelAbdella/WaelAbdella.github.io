10b1b0329090adf997579d3b67ac5bbd66cab097
Wael Ahmed <80431088+WaelAbdella@users.noreply.github.com>
2021-05-27 06:35:09 +0400

    a little delay on the loader

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-(function($){
-    'use strict';
-      $(window).on('load', function () {
-          if ($(".reveal-wrapper").length > 0)
-          {
-            TweenMax.to(".reveal-wrapper", 2, {
-                top: "-150%",
-                ease: Expo.easeInOut,
-                delay: 1//3.6
-            });
-          }
+(function ($) {
+  'use strict';
+  $(window).on('load', function () {
+    if ($('.reveal-wrapper').length > 0) {
+      TweenMax.to('.reveal-wrapper', 2, {
+        top: '-150%',
+        ease: Expo.easeInOut,
+        delay: 2, //3.6
       });
-  })(jQuery)
-
-
-
+    }
+  });
+})(jQuery);
 
 /*
 var tl = new TimelineMax();
@@ -34,21 +30,21 @@ tl.to(".loader", 1.6, {
 });
 */
 
-    //typing animation
+//typing animation
 
-    var typed = new Typed(".typing", {
-    strings: ["Developer", "Designer", "Creative"],
-    
-    typeSpeed: 40,
-    // backspacing speed
-    backSpeed: 20,
-    // time before backspacing
-    backDelay: 1000,
-    // loop
-    loop: true,
+var typed = new Typed('.typing', {
+  strings: ['Developer', 'Designer', 'Creative'],
+
+  typeSpeed: 40,
+  // backspacing speed
+  backSpeed: 20,
+  // time before backspacing
+  backDelay: 1000,
+  // loop
+  loop: true,
 });
-    
-    //typing animation
+
+//typing animation
 /*
     var typed = new Typed(".typingintro", {
     strings: ["Hi", "I am", "WAEL AHMED"],
@@ -61,70 +57,55 @@ tl.to(".loader", 1.6, {
 });
 */
 
-document.addEventListener("DOMContentLoaded", function(event) {
-var cursor = document.querySelector(".custom-cursor");
-var links = document.querySelectorAll("a");
-var initCursor = false;
+document.addEventListener('DOMContentLoaded', function (event) {
+  var cursor = document.querySelector('.custom-cursor');
+  var links = document.querySelectorAll('a');
+  var initCursor = false;
 
-for (var i = 0; i < links.length; i++) {
-var selfLink = links[i];
+  for (var i = 0; i < links.length; i++) {
+    var selfLink = links[i];
 
-selfLink.addEventListener("mouseover", function() {
-  cursor.classList.add("custom-cursor--link");
-});
-selfLink.addEventListener("mouseout", function() {
-  cursor.classList.remove("custom-cursor--link");
-});
-}
-
-window.onmousemove = function(e) {
-var mouseX = e.clientX;
-var mouseY = e.clientY;
-
-if (!initCursor) {
-  // cursor.style.opacity = 1;
-  TweenLite.to(cursor, 0.3, {
-    opacity: 1
-  });
-  initCursor = true;
-}
-
-TweenLite.to(cursor, 0, {
-  top: mouseY + "px",
-  left: mouseX + "px"
-});
-};
-
-window.onmouseout = function(e) {
-TweenLite.to(cursor, 0.3, {
-  opacity: 0
-});
-initCursor = false;
-};
-
-
-
-
-feather.replace();
-
-
-    $('.menu-btn').click(function(){
-    
-    $('.Nav-bar').toggleClass("active"); //gives the menu button functionality
-    $('.navigation').toggleClass("active"); //gives the menu button functionality
+    selfLink.addEventListener('mouseover', function () {
+      cursor.classList.add('custom-cursor--link');
     });
-    
+    selfLink.addEventListener('mouseout', function () {
+      cursor.classList.remove('custom-cursor--link');
+    });
+  }
 
+  window.onmousemove = function (e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
 
+    if (!initCursor) {
+      // cursor.style.opacity = 1;
+      TweenLite.to(cursor, 0.3, {
+        opacity: 1,
+      });
+      initCursor = true;
+    }
 
+    TweenLite.to(cursor, 0, {
+      top: mouseY + 'px',
+      left: mouseX + 'px',
+    });
+  };
 
+  window.onmouseout = function (e) {
+    TweenLite.to(cursor, 0.3, {
+      opacity: 0,
+    });
+    initCursor = false;
+  };
 
+  feather.replace();
 
+  $('.menu-btn').click(function () {
+    $('.Nav-bar').toggleClass('active'); //gives the menu button functionality
+    $('.navigation').toggleClass('active'); //gives the menu button functionality
+  });
 
-
-
-
-/*
+  /*
 
     
   function progress() {
@@ -151,80 +132,64 @@ $(document).on("scroll", progress);
 
 */
 
+  $(function () {
+    // Define window variables
 
+    var winScrollTop = $(window).scrollTop();
+    var winHeight = window.innerHeight;
+    var winWidth = window.innerWidth;
 
-    
+    // Define scene classes.
+    var sceneClass = 'scene';
+    var sceneActiveClass = sceneClass + '--active';
+    var sceneEndedClass = sceneClass + '--ended';
 
-
-
-
-
-$(function () {
-
-  // Define window variables
-
-  var winScrollTop = $(window).scrollTop();
-  var winHeight = window.innerHeight;
-  var winWidth = window.innerWidth;
-
-  // Define scene classes.
-  var sceneClass = 'scene';
-  var sceneActiveClass = sceneClass + '--active';
-  var sceneEndedClass = sceneClass + '--ended';
-
-  $(window).on('resize', function () {
+    $(window).on('resize', function () {
       winHeight = window.innerHeight;
       winWidth = window.innerWidth;
-  });
+    });
 
-  // Scene classes function.
-  function setScene($el) {
-
+    // Scene classes function.
+    function setScene($el) {
       // Get bounding values from section.
       var bounding = $el.data('elDom').getBoundingClientRect();
 
       if (bounding.top > winHeight) {
-
-          // Section is below the viewport.
-          // Section has not ended or started, therefore remove classes.
-          $el.find('.scene').removeClass(sceneActiveClass);
-          $el.find('.scene').removeClass(sceneEndedClass);
-
+        // Section is below the viewport.
+        // Section has not ended or started, therefore remove classes.
+        $el.find('.scene').removeClass(sceneActiveClass);
+        $el.find('.scene').removeClass(sceneEndedClass);
       } else if (bounding.bottom < 0) {
-
-          // Section is above the viewport.
-          // Section has ended, therefore remove classes.
-          $el.find('.scene').addClass(sceneEndedClass);
-          $el.find('.scene').removeClass(sceneActiveClass);
-
+        // Section is above the viewport.
+        // Section has ended, therefore remove classes.
+        $el.find('.scene').addClass(sceneEndedClass);
+        $el.find('.scene').removeClass(sceneActiveClass);
       } else {
+        // We're now inside the section, not below or above.
+        // If top of section is at top of viewport, add class active.
+        if (bounding.top <= 0) {
+          $el.find('.scene').addClass(sceneActiveClass);
+        }
 
-          // We're now inside the section, not below or above.
-          // If top of section is at top of viewport, add class active.
-          if (bounding.top <= 0) {
-              $el.find('.scene').addClass(sceneActiveClass);
-          }
+        // If top of section is below top of viewport, remove class active.
+        if (bounding.top > 0) {
+          $el.find('.scene').removeClass(sceneActiveClass);
+        }
 
-          // If top of section is below top of viewport, remove class active.
-          if (bounding.top > 0) {
-              $el.find('.scene').removeClass(sceneActiveClass);
-          }
+        // If bottom of section is at bottom of viewport, add class ended.
+        if (bounding.bottom <= winHeight) {
+          $el.find('.scene').addClass(sceneEndedClass);
+        }
 
-          // If bottom of section is at bottom of viewport, add class ended.
-          if (bounding.bottom <= (winHeight)) {
-              $el.find('.scene').addClass(sceneEndedClass);
-          }
-
-          // If bottom of section is not at bottom of viewport, remove class ended.
-          if (bounding.bottom > (winHeight)) {
-              $el.find('.scene').removeClass(sceneEndedClass);
-          }
+        // If bottom of section is not at bottom of viewport, remove class ended.
+        if (bounding.bottom > winHeight) {
+          $el.find('.scene').removeClass(sceneEndedClass);
+        }
       }
-  }
+    }
 
-  // This function sets up the horizontal scroll. This applies data attributes to the section for later use.
-  function setUpHorizontalScroll($el) {
-
+    // This function sets up the horizontal scroll. This applies data attributes to the section for later use.
+    function setUpHorizontalScroll($el) {
       var sectionClass = $el.attr('class');
 
       // Set content wrapper variables & data attributes.
@@ -239,7 +204,7 @@ $(function () {
 
       // Set right max variables & data attributes.
       var rightMax = $el.data('contentWrapperScrollWidth') - winWidth;
-      var rightMaxMinus = -(rightMax);
+      var rightMaxMinus = -rightMax;
       $el.data('rightMax', Number(rightMaxMinus));
 
       // Set initialized data variable to false do incidate scrolling functionality doesn't work yet.
@@ -262,20 +227,17 @@ $(function () {
 
       // Add class of init
       $el.addClass($el.attr('class') + '--init');
-  }
+    }
 
-  function resetHorizontalScroll($el) {
-    
-    
+    function resetHorizontalScroll($el) {
       // Update data attribute for content wrapper scroll width.
-  
-    var contentWrapperScrollWidth = $el.data('contentWrapperDom').scrollWidth;
+
+      var contentWrapperScrollWidth = $el.data('contentWrapperDom').scrollWidth;
       $el.data('contentWrapperScrollWidth', contentWrapperScrollWidth);
-    
 
       // Update rightMax variables & data attributes.
       rightMax = $el.data('contentWrapperScrollWidth') - winWidth;
-      rightMaxMinus = -(rightMax);
+      rightMaxMinus = -rightMax;
       $el.data('rightMax', Number(rightMaxMinus));
 
       // Update height of section to the scroll width of content wrapper.
@@ -289,18 +251,18 @@ $(function () {
 
       // If transform is smaller than rightmax, make it rightmax.
       if ($el.data('transformX') <= $el.data('rightMax')) {
-          $el.data('contentWrapper').css({
-              'transform': 'translate3d(' + $el.data('rightMax') + 'px, 0, 0)',
-          });
+        $el.data('contentWrapper').css({
+          transform: 'translate3d(' + $el.data('rightMax') + 'px, 0, 0)',
+        });
       }
-  }
+    }
 
-  var $horizontalScrollSections = $('.horizontal-scroll-section');
-  var $horizontalScrollSectionsTriggers = $horizontalScrollSections.find('.trigger');
+    var $horizontalScrollSections = $('.horizontal-scroll-section');
+    var $horizontalScrollSectionsTriggers =
+      $horizontalScrollSections.find('.trigger');
 
-  // Each function - set variables ready for scrolling functionality. Call horizontal scroll functions on load and resize.
-  $horizontalScrollSections.each(function (i, el) {
-    
+    // Each function - set variables ready for scrolling functionality. Call horizontal scroll functions on load and resize.
+    $horizontalScrollSections.each(function (i, el) {
       var $thisSection = $(this);
 
       $(this).data('elDom', $(this).get(0));
@@ -313,50 +275,57 @@ $(function () {
 
       // Resize function
       $(window).on('resize', function () {
-          // Reset horizontal scrolling data attributes and transform content wrapper if transform is bigger than scroll width.
-          resetHorizontalScroll($thisSection);
-          // Reset scene positioning.
-          setScene($thisSection);
+        // Reset horizontal scrolling data attributes and transform content wrapper if transform is bigger than scroll width.
+        resetHorizontalScroll($thisSection);
+        // Reset scene positioning.
+        setScene($thisSection);
       });
+    });
 
-  });
-
-  function setupHorizontalTriggers($el, section) {
+    function setupHorizontalTriggers($el, section) {
       var parent = $el.parent();
       var positionLeft = parent.position().left;
-      var positionLeftMinus = -(positionLeft);
+      var positionLeftMinus = -positionLeft;
       var triggerOffset = $el.data('triggerOffset');
-      triggerOffset = !triggerOffset ? 0.5 : triggerOffset = triggerOffset;
+      triggerOffset = !triggerOffset ? 0.5 : (triggerOffset = triggerOffset);
       $el.data('triggerOffset', triggerOffset);
       $el.data('triggerPositionLeft', positionLeftMinus);
       $el.data('triggerSection', section);
-  }
+    }
 
-  function useHorizontalTriggers($el) {
-      if ($el.data('triggerSection').data('transformX') <= ($el.data('triggerPositionLeft') * $el.data('triggerOffset'))) {
-          $el.data('triggerSection').addClass($el.data('class'));
+    function useHorizontalTriggers($el) {
+      if (
+        $el.data('triggerSection').data('transformX') <=
+        $el.data('triggerPositionLeft') * $el.data('triggerOffset')
+      ) {
+        $el.data('triggerSection').addClass($el.data('class'));
       } else {
-          if ($el.data('remove-class') !== false) {
-              $el.data('triggerSection').removeClass($el.data('class'));
-          }
+        if ($el.data('remove-class') !== false) {
+          $el.data('triggerSection').removeClass($el.data('class'));
+        }
       }
-  }
+    }
 
-  $horizontalScrollSectionsTriggers.each(function (i, el) {
-      setupHorizontalTriggers($(this), $(this).closest('.horizontal-scroll-section'));
-  });
+    $horizontalScrollSectionsTriggers.each(function (i, el) {
+      setupHorizontalTriggers(
+        $(this),
+        $(this).closest('.horizontal-scroll-section')
+      );
+    });
 
-  function transformBasedOnScrollHorizontalScroll($el) {
-
+    function transformBasedOnScrollHorizontalScroll($el) {
       // Get amount scrolled variables.
       var amountScrolledContainer = winScrollTop - $el.data('offsetTop');
-      var amountScrolledThrough = (amountScrolledContainer / ($el.data('outerHeight') - (winHeight - winWidth)));
+      var amountScrolledThrough =
+        amountScrolledContainer /
+        ($el.data('outerHeight') - (winHeight - winWidth));
 
       // Add transform value variable based on amount scrolled through multiplied by scroll width of content.
-      var toTransform = (amountScrolledThrough * $el.data('contentWrapperScrollWidth'));
+      var toTransform =
+        amountScrolledThrough * $el.data('contentWrapperScrollWidth');
 
       // Add transform value for minus (as we're transforming opposite direction).
-      var toTransformMinus = -(toTransform);
+      var toTransformMinus = -toTransform;
 
       // If transform value is bigger or equal than 0, set value to 0.
       toTransformMinus = Math.min(0, toTransformMinus);
@@ -369,48 +338,27 @@ $(function () {
 
       // If section has been initalized, apply transform.
       if ($el.data('initalized') == true) {
-          $el.data('contentWrapper').css({
-              'transform': 'translate3d(' + $el.data('transformX') + 'px, 0, 0)'
-          });
+        $el.data('contentWrapper').css({
+          transform: 'translate3d(' + $el.data('transformX') + 'px, 0, 0)',
+        });
       }
-  }
+    }
 
-  // 
-  $(window).on('scroll', function(){
-   
+    //
+    $(window).on('scroll', function () {
       // Get window scroll top.
       winScrollTop = $(window).scrollTop();
 
       // Each function in horizontal scroll sections.
       $horizontalScrollSections.each(function (i, el) {
-          transformBasedOnScrollHorizontalScroll($(this));
-          setScene($(this));
+        transformBasedOnScrollHorizontalScroll($(this));
+        setScene($(this));
       });
 
       // Each function for horizontal scroll section triggers.
       $horizontalScrollSectionsTriggers.each(function (i, el) {
-          useHorizontalTriggers($(this));
+        useHorizontalTriggers($(this));
       });
-               
+    });
   });
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
